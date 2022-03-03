c286f33bdec61f7982b8e87d67aa9d518857847e
Tanimara Santos <36615712+anthropovixen@users.noreply.github.com>
2022-03-03 11:04:32 +1100

    Create app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+export default function App() {
+  const [isGoingOut, setIsGoingOut] = useState(true);
+
+  function changeMind() {
+    setIsGoingOut((prevState) => !prevState);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="state">
+      <h1 className="state--title">Do I feel like going out tonight?</h1>
+      <div onClick={changeMind} className="state--value">
+        <h1>{isGoingOut ? "Yes" : "No"}</h1>
+      </div>
     </div>
   );
 }
-
-export default App;
